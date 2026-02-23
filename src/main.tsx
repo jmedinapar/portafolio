676877065f48442223e2f80619b10f572d8d16f4
Juan Medina <j.medina.par@gmail.com>
2026-02-23 15:16:04 -0500

    feat:finalize GA4 integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles/main.scss';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/main.scss'
 
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router.tsx';
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router.tsx'
+
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID)
+ReactGA.send('pageview')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
-);
+  </StrictMode>,
+)
+
